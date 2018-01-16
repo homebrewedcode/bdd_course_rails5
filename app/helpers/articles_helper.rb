a3314493f76c2961084d1e4dfc53ff14e8f338d8
Dale Pollitt <dale.negativezero@gmail.com>
2018-01-16 12:00:23 -0800

    implemented adding comments

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+  def persisted_comments(comments)
+    # note this is the same as comments.reject { |comment| comment.new_record? }
+    # quicker cooler 'colon pretzel' syntax
+    comments.reject(&:new_record?)
+  end
 end
